1725f3a8919acbfff104dd9f2bb9a57ed179a637
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2024-12-16 19:10:43 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -81,3 +81,7 @@ UPDATE_VERSION() {
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
+UPDATE_VERSION "zerotier"
+UPDATE_VERSION "softethervpn5"
+UPDATE_VERSION "xray-core"
+UPDATE_VERSION "cloudflared"
