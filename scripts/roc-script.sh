dab408610e6f0971abec5b05f6be56b382d492a5
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2024-12-17 18:15:42 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  #调整 softethervpn5版本 5187
-sed -i 's/PKG_VERSION:=5\.02\.5184/PKG_VERSION:=5\.02\.5187/' ./feeds/packages/net/softethervpn5/Makefile
-sed -i '/PKG_HASH:=/c\PKG_HASH:=c3ce6ae05ced6f61f28437f213117d1ce1838fbdda365f7dfd57f04dc1bd0b4b' ./feeds/packages/net/softethervpn5/Makefile
+sed -i 's/PKG_VERSION:=5\.02\.5184/PKG_VERSION:=5\.02\.5187/' ../feeds/packages/net/softethervpn5/Makefile
+sed -i '/PKG_HASH:=/c\PKG_HASH:=c3ce6ae05ced6f61f28437f213117d1ce1838fbdda365f7dfd57f04dc1bd0b4b' ../feeds/packages/net/softethervpn5/Makefile
 	
 #安装和更新软件包
 UPDATE_PACKAGE() {
